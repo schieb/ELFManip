9eaeacb555a1d4795cd043685a3e62ee13095510
schieb <bas120030@utdallas.edu>
2016-04-09 15:41:43 -0500

    remove arguments to main
    allow easy addition of more than one section

--- src/tester.py
+++ src/tester.py
@@ -9,17 +9,20 @@ from ELFManip import ELFManip
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print "Usage: %s [ELF file] [new section contents] [hexadecimal vma of new section]" % sys.argv[0]
+    if len(sys.argv) != 2:
+        #print "Usage: %s [ELF file] [new section contents] [hexadecimal vma of new section]" % sys.argv[0]
+        print "Usage: %s [ELF file]" % sys.argv[0]
         exit()
     
     elf_filename = sys.argv[1]
-    section_contents = sys.argv[2]
-    section_vma = sys.argv[3]
     
     elf = ELFManip(elf_filename)
     
-    elf.add_section(section_contents, sh_addr = int(section_vma, 16))
+    
+    section_contents = "/mnt/hgfs/GitHub/Delinker/tests/test_section"
+    section_vma = 0x09000000
+    
+    elf.add_section(section_contents, sh_addr = section_vma)
     
     elf.write_new_elf(elf.filename + ".new")
     
