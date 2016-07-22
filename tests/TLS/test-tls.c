ebd6eb1decaa58101eb8db220cd40d1d04ce1081
Erick Bauman <exb131030@utdallas.edu>
2016-07-22 18:54:52 -0500

    Added ability to add a new TLS section in test_tls.py

--- tests/TLS/test-tls.c
+++ tests/TLS/test-tls.c
@@ -9,9 +9,20 @@
 
 // THREAD DATA
 __thread int my_num; //.tbss
-__thread char thread_buf[BUF_SIZE];
+__thread char thread_buf[BUF_SIZE];//.tbss
+__thread char last_byte;//.tbss
 //__thread int *my_num_prt = &my_num; // reloc! (not possible)
+__thread char tdata_buf[] = "unique value";//.tdata
 __thread char *str = "rodata?"; // str in .rodata, ptr in .tdata (all point to same string)
+
+//Idea: what if I make the tdata section larger than it actually is and then overwrite the
+//dynamically loaded contents?
+
+//These all cause compilation error
+//__thread int tbss_num __attribute__ ((section ("specialtbss")));
+//__thread char *tdata_strptr __attribute__ ((section ("specialtdata"))) = "unique value";
+//__thread char tdata_str[] __attribute__ ((section ("specialtdata"))) = "in-thread string";
+
 
 // GLOBAL DATA
 unsigned int current = 0;
@@ -26,8 +37,21 @@ int get_my_num(void){
 void *do_thread_stuff(void *args){
 	pthread_mutex_lock(&lock);
 		my_num = current++;
+		char* gs;
+		asm ("movl %%gs:0, %0"
+			:"=r" (gs)
+			:
+			:);
+		printf("gs segment address: %p\n", gs);
 		printf("my_num %d @ %p\n", my_num, &my_num);
-		printf("str      @ %p\n", &str);
+		printf("gs - my_num (start of .tbss): 0x%x\n", (gs-(char*)&my_num) );
+		printf("str      @ %p, value %s\n", &str, str);
+		printf("tdata_buf@ %p, value %s\n", &tdata_buf, tdata_buf);
+		printf("gs - tdata_buf (start of .tdata): 0x%x\n", (gs-(char*)&tdata_buf) );
+		printf("thread_buf@ %p, value %s\n", &thread_buf, thread_buf);
+		printf("gs - last_byte (end of .tbss): 0x%x\n", (gs-(char*)&last_byte) );
+		printf("last_byte@ %p, value %c\n", &last_byte, last_byte);
+		get_my_num();
 		// the memset is critical. need to make sure the thread data
 		// doesnt overwrite the thread bookkeeping data
 		// if it does, we should get a segfault
--- tests/TLS/test_tls.py
+++ tests/TLS/test_tls.py
@@ -5,17 +5,82 @@
  If there is a .tbss sections, will the new section be loaded after *ALL OF* the .tbss section contents?
 '''
 import sys
-sys.path.append("../../src")
+sys.path.append("../../elfmanip")
 from Constants import *
 from ELFManip import ELFManip, Custom_Section, Custom_Segment
+from elftools.elf.elffile import ELFFile
 
 
 NUM_REQUESTED_SEGMENTS = 2
+
+tls_section_added = False
+tls_section_contents = b''
+tls_section_offset = 0
+
+def add_tls_section(fname,contents):
+    # This does not require ELFManip because it must
+    # be called earlier on, before we actually rewrite the
+    # binary, because I need the new TLS offset.
+    # We could obviously create the ELFManip object now, 
+    # but it won't be used again until we write it out at
+    # the end.
+    global tls_section_added
+    global tls_section_contents
+    tls_section_added = True
+    #Pad contents to 4-byte alignment
+    tls_section_contents = contents+('\0'*(4-len(contents)%4))
+    with open(fname) as f:
+        elf = ELFFile(f)
+       	for s in elf.iter_segments():
+            #Assume only one TLS segment exists (will fail on an already modified binary)
+            if s.header['p_type'] == 'PT_TLS':
+                tls_section_offset = s.header['p_memsz']+len(tls_section_contents)
+                print 'old section is 0x%x (%x with padding)'%(s.header['p_memsz'], s.header['p_memsz']+(4-s.header['p_memsz']%4))
+                print 'new content is 0x%x (%x with padding)'%(len(contents), len(contents)+(4-len(contents)%4))
+                print 'overall        0x%x (%x with padding)'%(tls_section_offset, tls_section_offset+(4-tls_section_offset%4))
+                return tls_section_offset + (4-tls_section_offset%4)
+    return len(contents) + (4-len(contents)%4) #If there is no TLS segment
+
+def get_tls_content(elf):
+    # For now assume that the TLS sections are adjacent and
+    # we can append their contents directly
+    # I also am assuming that there will probably be only
+    # two sections, .tdata and .tbss, which seems likely.
+    # This may work under different circumstances but it is
+    # hard to predict.
+    content = b''
+    if tls_section_added:
+        content+=tls_section_contents
+    print 'length of new contents: 0x%x'%len(content)
+    for entry in elf.shdrs['entries']:
+        if (entry.sh_flags & SHF_TLS) == SHF_TLS:
+            if entry.sh_type == SHT_NOBITS: # bss has no contents
+                content+='\0'*entry.sh_size # fill bss space with 0
+                print 'adding .tbss section of length: 0x%x'%entry.sh_size
+            else:
+                content+=entry.contents
+                print 'adding .tdata section of length: 0x%x'%len(entry.contents)
+    return content
 
 
 def main(filename):
     
     elf = ELFManip(filename, num_adtl_segments=NUM_REQUESTED_SEGMENTS)
+
+    new_data = "data that will hopefully go into thread local storage"
+    print 'ADDING TLS SECTION'
+
+    off = add_tls_section(filename,new_data)
+    print 'New data starting at offset %d (0x%x)'%(off,off)
+    print 'CONTENTS (length %d) ARE %s'%(len(tls_section_contents),tls_section_contents)
+
+    #print '---TLS SECTIONS---'
+    #print elf.shdrs['entries'][18] #.tdata
+    #print elf.shdrs['entries'][19] #.tbss
+
+    #elf.shdrs['entries'][18].sh_size = 0x17+0x100
+
+
     
     old_num_phdrs = elf.phdrs['max_num']
     new_phdr_offset = elf.relocate_phdrs()
@@ -25,17 +90,40 @@ def main(filename):
         exit()
     
     
-    newbytes_filename = './newbytes-tls'
-    with open(newbytes_filename, 'r') as f:
-        newbytes = f.read()
+    #newbytes_filename = './newbytes-tls'
+    #with open(newbytes_filename, 'r') as f:
+    #    newbytes = f.read()
+    newbytes = get_tls_content(elf)
+    print 'newbytes is %s'%newbytes
+    print 'newbyte length: 0x%x'%len(newbytes)
+
+    '''if len(newbytes)%4 != 0:
+        print 'newbytes not aligned %d'%len(newbytes)
+        newbytes+='\0'*(4-len(newbytes)%4) #Pad to 4-byte alignment
+        print 'newbytes now aligned %d'%len(newbytes)
+    print 'adding old tls data (plus bss?) (maybe because I don\'t give room for the bss section in the segment?)'
+    newbytes+=elf.shdrs['entries'][18].contents
+    #IF the .tdata section's end is aligned, no padding needs to go here.  IF!  If not, well, I need to check.
+    #newbytes+='\0' #Hard to determine how much padding needs to go here...
+    newbytes+='Z'*(elf.shdrs['entries'][19].sh_size)'''
+
+    #newbytes_filename2 = './newbytes-tls2'
+    #with open(newbytes_filename2, 'r') as f:
+    #    newbytes2 = f.read()
     
-    newbytes_section = Custom_Section(newbytes, sh_addr = 0x09000000, sh_flags= SHF_WRITE | SHF_ALLOC | SHF_EXECINSTR | SHF_TLS)
+    newbytes_section = Custom_Section(newbytes, sh_addr = 0x09000000, sh_type=SHT_PROGBITS, sh_flags= SHF_WRITE | SHF_ALLOC | SHF_TLS, sh_addralign=0x4)
+    #newbytes_section2 = Custom_Section(newbytes2, sh_addr =0x09001000, sh_type=SHT_NOBITS, sh_flags= SHF_WRITE | SHF_ALLOC | SHF_TLS)
     
     # make sure the section is loaded into memory so the loader doesnt fail on memcpy
     newbytes_segment = Custom_Segment(PT_LOAD)
     pt_load_segment = elf.add_segment(newbytes_segment)
     if pt_load_segment is not None:
         pt_load_section = elf.add_section(newbytes_section, newbytes_segment)
+
+    #newbytes_segment2 = Custom_Segment(PT_LOAD)
+    #pt_load_segment = elf.add_segment(newbytes_segment2)
+    #if pt_load_segment is not None:
+    #    pt_load_section = elf.add_section(newbytes_section2, newbytes_segment2)
     
     # normally, each section that belongs to a segment will be written to the ELF at write time.
     # however, this is special in that two segments are mapping the "same" section
@@ -47,6 +135,12 @@ def main(filename):
     pt_tls_segment = elf.add_segment(newbytes_segment)
     if pt_tls_segment is not None:
         pt_tls_section = elf.add_section(newbytes_section, newbytes_segment)
+
+    #newbytes_segment2 = Custom_Segment(PT_TLS, p_align=4)
+    #                                          Note the alignment. without this, it segfaults...
+    #pt_tls_segment = elf.add_segment(newbytes_segment2)
+    #if pt_tls_segment is not None:
+    #    pt_tls_section = elf.add_section(newbytes_section2, newbytes_segment2)
        
     # note that there are two section headers for the same section
     # this doesn't actually matter since the section headers are not used to load the ELF 
